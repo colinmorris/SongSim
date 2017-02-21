8478d488ba21cc83bf87c58b43dd79743d045b3f
colinmorris <colin.morris2@gmail.com>
2017-02-21 10:35:14 -0800

    remove some starter cruft

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Songsim from './Songsim.js';
 
@@ -8,12 +7,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>SongSim</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Songsim />
       </div>
     );
