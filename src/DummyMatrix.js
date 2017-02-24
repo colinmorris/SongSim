dca910b24fd2c0808e7a818ee38e394d92344b78
colinmorris <colin.morris2@gmail.com>
2017-02-24 00:26:37 -0800

    DummyMatrix

---
+++ src/DummyMatrix.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './Matrix.css';
+
+class DummyMatrix extends Component {
+
+  render() {
+    return (<svg className="matrix" viewBox="0 0 1 1" />);
+  }
+}
+
+export default DummyMatrix;
