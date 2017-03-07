ac423aae1b375d3cf121c6c92aeaee439b05841d
colinmorris <colin.morris2@gmail.com>
2017-03-07 12:15:32 -0800

    Use fixed DPI and scale matrix images up with CSS

--- pngify.sh
+++ pngify.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-imgsize=800
+#imgsize=800
 #dest=public/img/gallery/
 dest='./'
 for path in "$@"
 do
     fname=$(basename "$path")
     base="${fname%.*}"
-    inkscape -z -e "${dest}${base}.png" -w $imgsize $path
+    #inkscape -z -e "${dest}${base}.png" -w $imgsize $path
+    inkscape -z -e "${dest}${base}.png" $path
 done
