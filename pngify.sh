caf915d7cff861b1de6d5f1b9e3722bd52976885
colinmorris <colin.morris2@gmail.com>
2017-03-14 16:45:04 -0700

    pngify stuff

--- pngify.sh
+++ pngify.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#imgsize=800
+imgsize=800
 #dest=public/img/gallery/
-dest='./'
+dest='pngs/'
+dpi=180
 for path in "$@"
 do
     fname=$(basename "$path")
     base="${fname%.*}"
     #inkscape -z -e "${dest}${base}.png" -w $imgsize $path
-    inkscape -z -e "${dest}${base}.png" $path
+    inkscape -z -e "${dest}${base}.png" -d $dpi $path
 done
